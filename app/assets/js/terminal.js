342e5aeecda4be90cc83235b04112c5f8b760c22
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2024-01-03 18:24:15 +0100

    Enter, Backspace, Underscore

--- app/assets/js/terminal.js
+++ app/assets/js/terminal.js
@@ -68,5 +68,55 @@ window.addEventListener("keydown", function(event) {
     if(letter == "y") {log.innerHTML += letter}
     if(letter == "z") {log.innerHTML += letter}
 
+    if(letter == "A") {log.innerHTML += letter}
+    if(letter == "B") {log.innerHTML += letter}
+    if(letter == "C") {log.innerHTML += letter}
+    if(letter == "D") {log.innerHTML += letter}
+    if(letter == "E") {log.innerHTML += letter}
+    if(letter == "F") {log.innerHTML += letter}
+    if(letter == "G") {log.innerHTML += letter}
+    if(letter == "H") {log.innerHTML += letter}
+    if(letter == "I") {log.innerHTML += letter}
+    if(letter == "J") {log.innerHTML += letter}
+    if(letter == "K") {log.innerHTML += letter}
+    if(letter == "L") {log.innerHTML += letter}
+    if(letter == "M") {log.innerHTML += letter}
+    if(letter == "N") {log.innerHTML += letter}
+    if(letter == "O") {log.innerHTML += letter}
+    if(letter == "P") {log.innerHTML += letter}
+    if(letter == "Q") {log.innerHTML += letter}
+    if(letter == "R") {log.innerHTML += letter}
+    if(letter == "S") {log.innerHTML += letter}
+    if(letter == "T") {log.innerHTML += letter}
+    if(letter == "U") {log.innerHTML += letter}
+    if(letter == "V") {log.innerHTML += letter}
+    if(letter == "W") {log.innerHTML += letter}
+    if(letter == "X") {log.innerHTML += letter}
+    if(letter == "Y") {log.innerHTML += letter}
+    if(letter == "Z") {log.innerHTML += letter}
+    
+    if(letter == "Backspace") {
+        const backspace=document.getElementById("log").innerText;
+        backspace = backspace.substring(0, backspace.length - 1);
+        log.innerHTML = backspace;
+    }
+
+    if(letter == "Enter") {
+        const search =document.getElementById("log").innerText;
+        const box = document.getElementById('log');
+        // const underscore = this.document.getElementsByClassName('underscore');
+        // console.log(underscore);
+        const el = document.querySelector('.underscore');
+        el.classList.replace("underscore","blank");
+
+        box.setAttribute('id', 'logged');
+        // box.classList.replace("underscore","blank");
+        terminal.innerHTML +=
+        `
+        <div class="row">
+        <p style="margin-right: 8px;">PS C:\\Users/admin></p><p class="code" id="log"></p><p class="underscore">_</p>
+        </div>
+        `
+    }
 
 });
