b9065cefe349ff5d734eb36a850e42ce20e8c05f
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2024-01-24 11:26:42 +0100

    Working Json reader

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -11,6 +11,25 @@ function btnTheme(){
     })
 }
 
-fetch('https://server.com/data.json')
+fetch('https://raw.githubusercontent.com/Tijl-Pleuger-Vista/project-6/main/app/assets/json/test.json')
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((json) => {
+        box.innerHTML +=
+                `
+                <ul>
+                    <li>${json.category_subcategory[0].examples}</li>
+                    <li>${json.category_subcategory[0].level}</li>
+                    <li>${json.category_subcategory[0].problem}</li>
+
+                    <li>${json.category_subcategory[0].resources}</li>
+                    <li>${json.category_subcategory[0].risk}</li>
+                    <li>${json.category_subcategory[0].solution}</li>
+
+                    <li>${json.category_subcategory[0].summary}</li>
+                    <li>${json.category_subcategory[0].title}</li>
+                    <li>${json.category_subcategory[0].xp}</li>
+                </ul
+                
+                `
+        console.log(json)
+    });
