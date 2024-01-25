475073d484abe6b3d79910badd035f512899dd1a
jeerast <jeerast@gmail.com>
2024-01-25 04:39:27 -0800

    dot
    dot

--- PVO/PVO/MainWindow.xaml.cs
+++ PVO/PVO/MainWindow.xaml.cs
@@ -23,53 +23,48 @@ using System.Security.Cryptography.X509Certificates;
 using System.Reflection.Metadata;
 using Path = System.IO.Path;
 using System.Reflection;
+using static PVO.MainWindow;
 
 namespace PVO
 {
 
     public partial class MainWindow : Window
     {
-        //string solutionDir = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName;
-        //string testimg = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName;
-        string testimg = "C:\\Program Files (x86)\\Vista college\\LeenBook\\";
+        string imgs = "C:\\Program Files (x86)\\Vista college\\LeenBook\\";
         string solutionDir = "C:\\Program Files (x86)\\Vista college\\LeenBook\\";
 
 
 
 
 
-
+        //On App load
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            FirstIcon.Source = new BitmapImage(new Uri(testimg + "\\img\\World.png"));
-            SecondIcon.Source = new BitmapImage(new Uri(testimg + "\\img\\icons8-web-shield-48.png"));
-            ThirdIcon.Source = new BitmapImage(new Uri(testimg + "\\img\\icons8-local-network-64.png"));
-            FourthIcon.Source = new BitmapImage(new Uri(testimg + "\\img\\icons8-security-configuration-48.png"));
+            FirstIcon.Source = new BitmapImage(new Uri(imgs + "\\img\\World.png"));
+            SecondIcon.Source = new BitmapImage(new Uri(imgs + "\\img\\icons8-web-shield-48.png"));
+            ThirdIcon.Source = new BitmapImage(new Uri(imgs + "\\img\\icons8-local-network-64.png"));
+            FourthIcon.Source = new BitmapImage(new Uri(imgs + "\\img\\icons8-security-configuration-48.png"));
 
 
 
 
 
         }
-        private void Close_App(object sender, RoutedEventArgs e)
-        {
-            Close();
 
-
-        }
-
-
+        /*Json names (firstlayer)*/
         public class Person
-        { /*Json names (firstlayer)*/
+        { 
             public string? FirstName { get; set; }
             public string? LastName { get; set; }
             public string? JobTitle { get; set; }
 
-            public Category_subcategory[]? category_subcategory1 { get; set; }
+            public Category_subcategory[]? category_subcategory { get; set; }
 
         }
+
+        /*Json names (Secondlayer)*/
         public class Category_subcategory
-        { /*Json names (Secondlayer)*/
+        {
             public string? title { get; set; }
             public string? level { get; set; }
             public string? risk { get; set; }
@@ -82,18 +77,6 @@ namespace PVO
 
         }
 
-
-        public static void Test()
-            {
-            string fileName = "person.json";
-            string path = Path.Combine(Environment.CurrentDirectory, @"Json\", fileName);
-            string text = File.ReadAllText(path);
-                var person = JsonSerializer.Deserialize<Person>(text);
-
-
-        }
-
-
         public void Internet(object sender, RoutedEventArgs e)
         {
             //read the json
@@ -102,22 +85,28 @@ namespace PVO
             //deserialise(make it into string for the person class variables)
             var person = JsonSerializer.Deserialize<Person>(text);
 
-
-
-
+            ClearMainText();
 
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
-            this.MainText.Text = person.JobTitle;
+
+            this.OpeningMessage.Text = null;
+            this.MainTitleText.Text = person.category_subcategory[0].title;
+            this.InfoText.Text = person.category_subcategory[0].summary;
+            this.ProblemText.Text = person.category_subcategory[0].problem;
+            this.SolutionText.Text = person.category_subcategory[0].solution;
+            this.ExamplesText.Text = person.category_subcategory[0].examples;
+            this.ResourcesText.Text = person.category_subcategory[0].resources;
+
+            this.MainTitle.Text = "Title";
+            this.InfoTitle.Text = "Summary";
+            this.ProblemTitle.Text = "Problem";
+            this.SolutionTitle.Text = "Solution";
+            this.ExamplesTitle.Text = "Examples";
+            this.ResourcesTitle.Text = "Resources";
+
+
+
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-            this.MainText.Text = person.category_subcategory1[0].title;
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
-
-
-
-
-
-
 
 
 
@@ -129,20 +118,15 @@ namespace PVO
 
             var person = JsonSerializer.Deserialize<Person>(text);
 
+            ClearMainText();
+
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
-            this.MainText.Text = person.LastName;
+            this.MainTitleText.Text = person.LastName;
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
 
         }
 
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-
-
+        //Enter key does something
         public async void OnKeyDown(object sender, KeyEventArgs e)
         {
             
@@ -156,35 +140,48 @@ namespace PVO
             }
         }
 
-
+        //ChatBot
         public async Task chatr()
         {
+            try
+            {
+                string user_input = UserInput.Text;
 
-            string user_input = UserInput.Text;
+                //authApi
+                var authentication = new APIAuthentication("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");
+                var api = new OpenAIAPI(authentication);
 
-            //authApi
-            var authentication = new APIAuthentication("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");
-            var api = new OpenAIAPI(authentication);
+                // Start a new chat
+                var conversation = api.Chat.CreateConversation();
 
-            // Start a neew chat
-            var conversation = api.Chat.CreateConversation();
+                // Add user input and receive a reply from ChatGPT
+                conversation.AppendUserInput(user_input);
 
-            // Add user input and receive a reply from ChatGPT
-            conversation.AppendUserInput(user_input);
+                var response = await conversation.GetResponseFromChatbotAsync();
 
-            var response = await conversation.GetResponseFromChatbotAsync();
-            
-            Chatbox.Text = response;
-
+                Chatbox.Text = response;
+            } catch {
+                Chatbox.Text = "Sorry at this moment in time i am not available. Please contact an admin";
+            }
 
 
         }
-        private void Icons(object sender, RoutedEventArgs e)
+
+        public void ClearMainText()
         {
-            
-            FirstIcon.Source = new BitmapImage(new Uri(testimg));
-
-
+            this.OpeningMessage.Text = null;
+            this.MainTitleText.Text = null;
+            this.InfoText.Text = null;
+            this.ProblemText.Text = null;
+            this.SolutionText.Text = null;
+            this.ExamplesText.Text = null;
+            this.ResourcesText.Text = null;
+            this.MainTitle.Text = null;
+            this.InfoTitle.Text = null;
+            this.ProblemTitle.Text = null;
+            this.SolutionTitle.Text = null;
+            this.ExamplesTitle.Text = null;
+            this.ResourcesTitle.Text = null;
         }
 
 
